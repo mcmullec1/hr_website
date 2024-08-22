fbb6a766405ad9b227c9f814f8254f15ee9272c5
clare-mac <mcmclare011@gmail.com>
2024-08-22 14:07:15 +0100

    cleaning code and adding comments

--- src/DownloadButton.jsx
+++ src/DownloadButton.jsx
@@ -5,6 +5,7 @@ import download from "/download.png"
 
 function DownloadButton({colours, overallSessionData}) {
 
+    //define the column names for the data
     const header_names = [
         'timeInterval',
         'hr',
@@ -12,16 +13,16 @@ function DownloadButton({colours, overallSessionData}) {
         'id'
       ];
 
+    //to convert object data to CSV format by delimiting
     function jsonToCSV(data, header_names) {
 
-        if (data.length === 0) {
-            return '';
-        }
+        if (data.length === 0) {return '';}
 
         const headers = header_names.join(',') + '\n';
 
+        //join data in correct header order with commas, then end with new line
         const rows = data.map((row) => {
-            return header_names.map((field) => String(row[field]) || '').join(',');
+            return header_names.map((item) => String(row[item]) || '').join(',');
             })
             .join('\n');
 
@@ -29,22 +30,28 @@ function DownloadButton({colours, overallSessionData}) {
       }
 
 
-    
+    //function to run when download button is clicked
     const handleDownload = (data, header_names) => {
-        const csvData = jsonToCSV(data, header_names);
 
-        if (csvData === '') {
-        alert('No data to export');
+        //get CSV string data
+        const csv_data = jsonToCSV(data, header_names);
+
+        //if no data alert
+        if (csv_data === '') {
+          alert('Export failed.');
         }
         
+        //create CSV item with blob, create link on page and click it to download, then remove
         else {
-        const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.setAttribute('download', 'heartrate_data.csv');
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+
+          const blob = new Blob([csv_data], { type: 'text/csv;charset=utf-8;' });
+          
+          const link = document.createElement('a');
+          link.href = URL.createObjectURL(blob);
+          link.setAttribute('download', 'heartrate_data.csv');
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
         }
     }
 
@@ -52,21 +59,17 @@ function DownloadButton({colours, overallSessionData}) {
         <>
         <button className='download_button' onClick={() => handleDownload(overallSessionData, header_names)}>
           <Box
-          height="100%"
-          width="100%"
-          backgroundColor={colours["light_green"]}
-          borderRadius="100%"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          //marginRight="20px"
-          //marginLeft="64px"
+            height="100%"
+            width="100%"
+            backgroundColor={colours["light_green"]}
+            borderRadius="100%"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
           >
             <Box
               component="img"
-              sx={{
-                height: "30px",
-              }}
+              sx={{height: "30px",}}
               alt="Download symbol."
               src={download}
             ></Box>
