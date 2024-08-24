9ee0568e865fd42a182092ab3a727c89c5de0607
clare-mac <mcmclare011@gmail.com>
2024-08-24 16:12:12 +0100

    chainging grid layout

--- src/Monitors.jsx
+++ src/Monitors.jsx
@@ -11,13 +11,18 @@ function Monitors({monitor_count, timeInterval, colours, sendData}) {
             container
             width="100%"
             spacing={4}
-            alignItems="center"
-            justifyContent="space-around"
             >
             {/* Create a Monitor component for each number in monitor count and pass props */}
             {Array.from(Array(monitor_count).keys()).map((i, index) => {
                 return (
-                    <Grid item key={i}><Monitor timeInterval={timeInterval} colours={colours} sendData={sendData} id={i}/></Grid>
+                    <Grid 
+                        item xs={12} sm={12} md={6} lg={4} key={i}
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Monitor timeInterval={timeInterval} colours={colours} sendData={sendData} id={i}/>
+                    </Grid>
                 );
             })}
         </Grid>
